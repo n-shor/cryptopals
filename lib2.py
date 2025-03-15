2fe3c950c7f14dc07caec3b018ec3ed5f6c2e806
n-shor <80694562+n-shor@users.noreply.github.com>
2025-03-15 14:12:28 +0200

    added PKCS#7 padding

--- lib2.py
+++ lib2.py
@@ -1,1 +1,10 @@
 from lib1 import *
+
+# Challenge 9
+
+def pkcs7_padding(block: bytes, length: int):
+    padding = length - len(block)
+    print(padding)
+    if padding < 0:
+        raise ValueError("Block length must not be larger than padding size")
+    return block + padding * padding.to_bytes()
