70813b522dfc6ca70234f8ece0a87dd91cd5d477
n-shor <80694562+n-shor@users.noreply.github.com>
2025-04-03 02:07:54 +0300

    added challenge 12

--- lib2.py
+++ lib2.py
@@ -1,6 +1,7 @@
 from lib1 import *
 import os
 import random
+from collections.abc import Callable
 
 def split_to_blocks(data: bytes, size: int=16) -> list[bytes]:
     return [data[i:i + size] for i in range(0, len(data), size)]
@@ -80,26 +81,69 @@ def generate_random_bytes(size: int=16) -> bytes:
     return os.urandom(size)
 
 
-def encryption_oracle(data: bytes) -> bytes:
-    start_padding = os.urandom(random.randint(5, 10))
-    end_padding = os.urandom(random.randint(5, 10))
-    key = generate_random_bytes()
-    padded_data = start_padding + data + end_padding
+def encryption_oracle(data: bytes, use_ecb : int = random.randint(0, 1), key : bytes = generate_random_bytes(), no_padding : bool = False) -> bytes:
+    if not no_padding:
+        start_padding = os.urandom(random.randint(5, 10))
+        end_padding = os.urandom(random.randint(5, 10))
+        data = start_padding + data + end_padding
+    
 
-    # 0 is ECB, 1 is CBC
-    if random.randint(0, 1) == 1:
-        return encrypt_aes_128_ecb(padded_data, key)
+    # 1 is ECB, 0 is CBC
+    if use_ecb:
+        return encrypt_aes_128_ecb(data, key)
     else:
-        return encrypt_aes_128_cbc(generate_random_bytes(), padded_data, key)
+        return encrypt_aes_128_cbc(generate_random_bytes(), data, key)
 
 
-def detect_ecb_or_cbc():
+def is_ecb(data: bytes, use_ecb : int = random.randint(0, 1)) -> bool:
     for _ in range(10):
-        encrypted = encryption_oracle(b"a" * 16 * 3) # need at least 3 blocks to ensure at least # 2 blocks of only b'a' exist after padding
+        encrypted = encryption_oracle(b"a" * 16 * 3 + data, use_ecb=use_ecb) # need at least 3 blocks to ensure at least # 2 blocks of only b'a' exist after padding
         blocks = split_to_blocks(encrypted)
 
         if len(set(blocks)) != len(blocks):
-            print("Detected ECB")
+            return True
         else:
-            print("Detected CBC")
+            return False
 
+
+# Challenge 12
+
+def find_oracle_block_size(oracle : Callable) -> int:
+    base = oracle(b"a" * 1, no_padding = True) # No padding cuz idk how to do it otherwise
+    enc = base
+    
+    i = 2
+    while (len(base) == len(enc)):
+        enc = oracle(b"a" * i) # No padding cuz idk how to do it otherwise
+        i += 1
+
+    return len(enc) - len(base)
+
+
+key = generate_random_bytes()
+
+def ecb_pad_encrypt(data: bytes, key: bytes) -> bytes:
+    padding = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK"
+    return encrypt_aes_128_ecb(data + base64_to_bytes(padding), key)
+
+
+def ecb_pad_decrypt(key: bytes) -> bytes:
+    "This only takes a key because ecb_pad_encrypt needs to take a key, we could make that function not take a key and have it as a hardcoded variable there"
+    "and in that case we would not need to have this function take a key"
+    encrypted = ecb_pad_encrypt(b"A" * 64, key)
+    block_size = find_oracle_block_size(encryption_oracle)
+    block_count = len(ecb_pad_encrypt(b"", key)) // block_size + 1
+    using_ecb = is_ecb(encrypted)
+
+    decrypted = b""
+    for block_index in range(block_count):
+        for i in range(block_size):
+            baseline = ecb_pad_encrypt(b"A" * (block_size - 1 - i), key)[block_index * block_size:(block_index + 1) * block_size]
+            for c in range(256):
+                cmp = ecb_pad_encrypt(b"A" * (block_size - 1 - i) + decrypted + c.to_bytes(), key)[block_index * block_size:(block_index + 1) * block_size]
+                if cmp == baseline:
+                    decrypted += c.to_bytes()
+                    break
+                
+    return pkcs7_unpadding(decrypted)
+    
